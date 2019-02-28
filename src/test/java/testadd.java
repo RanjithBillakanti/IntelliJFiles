e743635c49fabe89b451d060cf824275acd0681b
Ranjithkumar.B <ranjitbillakanti@gmail.com>
2019-02-28 10:19:13 +0530

    test project to commit to git hub....

--- src/test/java/testadd.java
+++ src/test/java/testadd.java
@@ -3,6 +3,7 @@ public class testadd
     public static void main(String args[])
     {
         System.out.println("This is the first git hub program pushed from IntelliJ");
+        System.out.println("This is the second git hub program pushed from IntelliJ");
     }
 
 }
